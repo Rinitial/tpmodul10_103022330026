1ce3eafd33c07ad61e0c888739185c86fd6b64a2
Raditya <mp.raditya93w@gmail.com>
2025-04-27 10:49:54 +0700

    Menambahkan API Mahasiswa

---
+++ tpmodul10_103022330026/Controllers/MahasiswaController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using tpmodul10_103022330026.NewFolder;
+using System.Collections.Generic;
+
+namespace tpmodul10_103022330026.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MahasiswaController : ControllerBase
+    {
+        private static List<Mahasiswa> mahasiswaList = new List<Mahasiswa>
+        {
+            new Mahasiswa {Nama  = "Raditya", Nim = "103022330026" },
+            new Mahasiswa {Nama = "Daniyal", Nim = "1030223" },
+            new Mahasiswa {Nama = "Admiral", Nim = "1030223" },
+            new Mahasiswa {Nama = "Azka", Nim = "1030223" }
+        };
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Mahasiswa>> Get()
+        {
+            return mahasiswaList;
+        }
+
+        [HttpGet("{index}")]
+        public ActionResult<Mahasiswa> Get(int index)
+        {
+            if (index < 0 || index >= mahasiswaList.Count)
+            {
+                return NotFound("Mahasiswa tidak ditemukan.");
+            }
+            return mahasiswaList[index];
+        }
+
+        [HttpPost]
+        public ActionResult<IEnumerable<Mahasiswa>> Post([FromBody] Mahasiswa mahasiswaBaru)
+        {
+            mahasiswaList.Add(mahasiswaBaru);
+            return mahasiswaList;
+        }
+
+        [HttpDelete("{index}")]
+        public ActionResult<IEnumerable<Mahasiswa>> Delete(int index)
+        {
+            if (index < 0 || index >= mahasiswaList.Count)
+            {
+                return NotFound("Mahasiswa tidak ditemukan.");
+            }
+            mahasiswaList.RemoveAt(index);
+            return mahasiswaList;
+        }
+    }
+}
---
+++ tpmodul10_103022330026/NewFolder/Mahasiswa.cs
@@ -0,0 +1,8 @@
+﻿namespace tpmodul10_103022330026.NewFolder
+{
+    public class Mahasiswa
+    {
+        public string Nama { get; set;  }
+        public string Nim { get; set;  }
+    }
+}
